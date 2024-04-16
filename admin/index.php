e5f2a17bef781af7c384ad98435187d6dc6a0e65
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2024-04-16 12:54:19 +0100

    started the admin

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,28 @@
-<?php
-    echo "Hello Admin";
-?>
+<html>
+    <head>
+        <title>Online Food Order</title>
+
+        
+    </head>
+    
+    <body>
+        <!-- Menu Section Starts -->
+        <div class="menu">
+            Munu
+        </div>
+        <!-- Menu Section Ends -->
+
+        <!-- main Content Section Starts -->
+        <div class="main content">
+            content
+        </div>
+        <!-- Menu Content Section End -->
+
+        <!-- Footer Section Starts -->
+        <div class="footer">
+            footer
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
