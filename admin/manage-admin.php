e2e7ce7c0d56481923964a80c2b1ce6adcd2172c
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2024-04-16 15:22:25 +0100

    admin manage file is added with change, update, delete button

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -4,6 +4,25 @@
         <div class="main-content">
             <div class ="wrapper">
                 <h1>Administrator Dashboard</h1>
+                <table class="tbl-full">
+                    <tr>
+                        <th>S.N.</th>
+                        <th>Full Name</th>
+                        <th>Username</th>
+                        <th>Actions</th>
+                    </tr>
+
+                    <tr>
+                        <td>1.</td>
+                        <td>Sudath Nawagamuwage</td>
+                        <td>Sudath</td>
+                        <td>
+                            <a href="#" class="btn-primary">Change Password</a>
+                            <a href="#" class="btn-secondary">Update Admin</a>
+                            <a href="#" class="btn-danger">Delete Admin</a>
+                        </td>
+                    </tr>
+                </table>
             </div>           
         </div>
         <!-- Menu Content Section End -->
