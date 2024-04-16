fe6050505a3f79f7a2a02f1215bb2edaeb4a3982
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2024-04-16 13:12:17 +0100

    added

--- admin/index.php
+++ admin/index.php
@@ -2,25 +2,31 @@
     <head>
         <title>Online Food Order</title>
 
-        
+        <link rel="stylesheet" href="../css/admin.css">
     </head>
     
     <body>
         <!-- Menu Section Starts -->
         <div class="menu">
-            Munu
+            <div class ="wrapper">
+                Menu
+            </div>            
         </div>
         <!-- Menu Section Ends -->
 
         <!-- main Content Section Starts -->
         <div class="main content">
-            content
+            <div class ="wrapper">
+                content
+            </div>           
         </div>
         <!-- Menu Content Section End -->
 
         <!-- Footer Section Starts -->
         <div class="footer">
-            footer
+            <div class ="wrapper">
+                <p class="text-center"> 2024 All right reservied, Spice Ceylon Restaurant. Developed by - <a href="#">Sudath Nawagamuwage</a></p> 
+            </div>           
         </div>
         <!-- Footer Section Ends -->
 
