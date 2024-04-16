782860ee7a375e0628986e18a8b9932ccc881f4d
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2024-04-16 14:34:27 +0100

    created  mamage-admin.php

---
+++ admin/manage-admin.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <title>Online Food Order</title>
+
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+    
+    <body>
+        <!-- Menu Section Starts -->
+        <div class="menu text-center">
+            <div class ="wrapper">
+            <ul>
+                <li><a href="index.php">Dashboard</a></li>
+                <li><a href="manage-category.php">Category</a></li>
+                <li><a href="manage-food.php">Food Items</a></li>
+                <li><a href="manage-order.php">Order Section</a></li>
+                <li><a href="manage-admin.php">Manage Admin</a></li>
+                <li><a href="logout.php">Logout</a></li>
+                </ul>
+            </div>            
+        </div>
+        <!-- Menu Section Ends -->
+
+        <!-- main Content Section Starts -->
+        <div class="main-content">
+            <div class ="wrapper">
+                <h1>Administrator Dashboard</h1>
+            </div>           
+        </div>
+        <!-- Menu Content Section End -->
+
+        <!-- Footer Section Starts -->
+        <div class="footer">
+            <div class ="wrapper">
+                <p class="text-center"> 2024 All right reservied, Spice Ceylon Restaurant. Developed by - <a href="#">Sudath Nawagamuwage</a></p> 
+            </div>           
+        </div>
+        <!-- Footer Section Ends -->
+    </body>
+</html>
